eed1e3418ebed68548b3654408af019af8aa982d
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-11 14:31:30 +0800

    the fun part

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+const morse = {
+   "A": "•-",
+   "B": "-•••",
+   "C": "-•-•",
+   "D": "-••",
+   "E": "•",
+   "F": "••-•",
+   "G": "--•",
+   "H": "••••",
+   "I": "••",
+   "J": "•---",
+   "K": "-•-",
+   "L": "•-••",
+   "M": "--",
+   "N": "-•",
+   "O": "---",
+   "P": "•--•",
+   "Q": "--•-",
+   "R": "•-•",
+   "S": "•••",
+   "T": "-",
+   "U": "••-",
+   "V": "•••-",
+   "W": "•--",
+   "X": "-••-",
+   "Y": "-•--",
+   "Z": "--••"
+};
+
+function convert(str){
+    return str.toUpperCase().split("").map(char => {
+        return morse[char] ? morse[char] : char;
+    }).join("");
+}
+
+Events.on(ClientLoadEvent, e => {
+    if(Core.settings.getString("locale") == "en"){
+        const bundleCopy = Core.bundle.getProperties().copy();
+        Core.bundle.getProperties().each((k, v) => {
+            const newV = convert(v);
+            bundleCopy.put(k, newV);
+        });
+        Core.bundle.setProperties(bundleCopy);
+        Vars.content.each(c => {
+            if(!(c instanceof UnlockableContent)) return;
+            c.localizedName = convert(c.localizedName);
+            if(c.description != null) c.description = convert(c.description);
+            if(c.details != null) c.details = convert(c.details);
+        });
+    }
+});
